764959e40ce6e169c7074dbf4891de1ec99dd69b
kevinparker-dev <muhammadkamil.raza@launchbox.pk>
2025-05-30 22:57:59 +0500

    udpated

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,8 @@ import { ErrorToast } from "./components/global/Toaster"; // Import your toaster
 import Cookies from "js-cookie";
 // import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
-// export const baseUrl = "https://api.starlitdating.com";
-export const baseUrl = "http://192.168.100.226:7000";
+export const baseUrl = "https://api.starlitdating.com";
+// export const baseUrl = "http://192.168.100.226:7000";
 
 // export const baseUrl = "https://155e-45-199-187-86.ngrok-free.app";
 
